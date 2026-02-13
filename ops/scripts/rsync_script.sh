f2396dcd0158301466a3686cb550a7e954f625a4
ThomasJager99 <artemzubchenko1@gmail.com>
2026-02-13 13:30:26 +0100

    After setting down MYSQL container, i added mysqldump in rsync_backup script

--- ops/scripts/rsync_script.sh
+++ ops/scripts/rsync_script.sh
@@ -7,15 +7,29 @@ INFRA="/home/user/dir"
 DATA="/home/user/application.data"
 
 mkdir -p "$BACKUP_BASE/infra" "$BACKUP_BASE/app_data"
+mkdir -p "$DATA/mysql_backup"
+
 mountpoint -q "$BACKUP_BASE" || exit 1
 
 {
 echo "=== BACKUP START $(date) ==="
 
-/usr/bin/rsync -avh --delete --itemize-changes "$INFRA/" "$BACKUP_BASE/infra/"
+echo "--- mysql dump ---"
+/usr/bin/docker exec mysql_db \
+  sh -c 'mysqldump -u root -p"$MYSQL_ROOT_PASSWORD" appdb' \
+  > "$DATA/mysql_backup/backup_mysql_$(date +%F_%H-%M-%S).sql"
+
+echo "--- mysql dump done ---"
+
+/usr/bin/rsync -avh --delete --itemize-changes \
+"$INFRA/" "$BACKUP_BASE/infra/"
+
 echo "--- infra rsync done ---"
 
-/usr/bin/rsync -avh --delete --itemize-changes "$DATA/" "$BACKUP_BASE/app_data/"
+/usr/bin/rsync -avh --delete --itemize-changes \
+--exclude 'mysql_data/' \
+"$DATA/" "$BACKUP_BASE/app_data/"
+
 echo "--- data rsync done ---"
 
 echo "=== BACKUP END $(date) ==="
